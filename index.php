362a991dbc03c4bd2f49260ca6faeebead90bd20
Lclarke277 <Lclarke@unca.edu>
2017-01-22 19:25:43 -0500

    Started Index CSS

--- index.php
+++ index.php
@@ -5,30 +5,48 @@
 <script src="jquery-3.1.1.min.js"></script> 
 <script src="register.js"></script>
 <script src="login.js"></script>
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
 </head>
 <body>
-
-<span id='register_message'></span>    
     
-<form id='register_form' method='post' action='register.php'>
-    First: <input type='text' name='first_name' maxlength='30' required='required'><br>
-    Last: <input type='text' name='last_name' maxlength='30' required='required'><br>
-    Email: <input id='email_register' type='text' name='email' maxlength='60' required='required'><span id='email_exists'></span><br>
-    Password: <input type='password' name='password' maxlength='60' required='required'><br>
-    <input type='submit' id='register_user' name='register' value='Register'>
-</form>
+    <div id='wrap'>
 
-    <br>
+        <div id="header">
 
-<span id='login_message'></span>
-    
-<form id='login_form' method='post' action='login.php'>
-    Email: <input id='login_email' type='text' name='email' maxlength='60' required='required'><br>
-    Password: <input id='login_pass' type='password' name='password' maxlength='60' required='required'><br>
-    <input type='submit' name='login' value='Log In'>
-</form>
-    
-  
+        <img id='logo' src="images/site/logo.png">
+
+        <span id='login_message'></span>
+
+        <form id='login_form' method='post' action='login.php'>
+            <input id='login_email' type='text' name='email' maxlength='60' required='required' placeholder="Email">
+            <input id='login_pass' type='password' name='password' maxlength='60' required='required' placeholder="Password">
+            <input type='submit' name='login' value='Log In'>
+        </form>    
+        </div>
+
+        <div id='main_body'>
+
+            <span id='register_message'></span>    
+
+            <div id='register_wrap'>
+
+            <h2 id='register'>Sign Up</h2>
+
+            <form id='register_form' method='post' action='register.php'>
+                <input class='register' type='text' name='first_name' maxlength='30' required='required' placeholder="First Name">
+                <input class='register' type='text' name='last_name' maxlength='30' required='required' placeholder="Last Name">
+                <input class='register' id='email_register' type='text' name='email' maxlength='60' required='required' placeholder="Email"><span id='email_exists'></span>
+                <input class='register' type='password' name='password' maxlength='60' required='required' placeholder="Password">
+                <input class='register' type='submit' id='register_user' name='register' value='Register'>
+            </form>
+                
+            </div><!-- Register Wrap -->
+
+
+
+        </div><!-- Main Body -->    
+    </div><!-- Wrap -->  
     
 </body>         
 </html>
