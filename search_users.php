0ab724617b26bd2b85c613aca17e0a931633d934
Lclarke277 <Lclarke@unca.edu>
2017-01-23 20:49:23 -0500

    Cleaning Up Search
    Need to account for profile_wrap technique to display images instead of
    img src. Better yet look into cropping images on upload if a simple
    solution

--- search_users.php
+++ search_users.php
@@ -12,16 +12,23 @@ $current_user = $_SESSION['user_id'];
 $search = "%" . $_POST['user_search'] . "%";
 
 // Searches by users first or last name, can be changed later
-$search_stmt = $conn->prepare("SELECT user_id, first_name, last_name from users WHERE (first_name LIKE ? OR last_name LIKE ?) AND user_id != ?");
+$search_stmt = $conn->prepare("SELECT user_id, first_name, last_name, email from users WHERE (first_name LIKE ? OR last_name LIKE ?) AND user_id != ?");
 $search_stmt->bind_param('ssi', $search, $search, $current_user);
 $search_stmt->execute();
 $search_stmt->store_result();
 
 // SELECT (bind_result) FROM users...
-$search_stmt->bind_result($search_user_id, $search_first, $search_last);
+$search_stmt->bind_result($search_user_id, $search_first, $search_last, $search_email);
 
 while($search_stmt->fetch()) {
-    echo "<p>" . $search_first . " " . $search_last . "<br></p>";
+    echo "<div class='search_wrap'>
+        <img class='search_image' src='images/$search_user_id/profile.jpg'>
+        <div class='search_bio'>
+            <h3 class='search'>" . $search_first . " " . $search_last . "</h3>
+                <hr>
+            <p class='search'>" . $search_email . "</p>
+        </div>
+    <div>";
 };
 
 
